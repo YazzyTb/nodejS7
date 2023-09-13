b8a06bfbb8b71022d103af559fad8a1e4a148c01
YazzyTb <133922590+YazzyTb@users.noreply.github.com>
2023-09-12 23:32:22 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,20 @@
-const http = require ('http');
+const express = require('express')
+const app = express()
 
-http.createServer((req,res)=>{
-   
-//    res.writeHead(200, {'Content-Type': 'text/plain'})  // Archivos txt
-   
-//    res.writeHead(200, {'Content-Type': 'application/json'})  // lo muestra como codigo json 
+//servir contenido estatico
+app.use(express.static('public'))
 
-
-    res.setHeader('Content-Disposition','attachment; filename=lista.csv');  // añade a los Header hacer un archivo que se va a descargar y ese es el nombre del archivo 'lista.csv'
-   res.writeHead(200, {'Content-Type': 'application/csv'})  //archivo excel u archivo csv 
-
-    // const persona ={
-    //     id: 1,
-    //     nombre: 'Fernando'
-    // }
-
-   // res.write(JSON.stringify (persona));
-   
-   res.write('id, nombre\n');
-   res.write('1, Ana\n');
-   res.write('2, Carlos\n');
-   res.write('3, Marcelo\n');
-   res.write('4, Paola\n');
-
-   res.end();
+app.get('/', function (req, res) {
+  res.send('Home page')
 })
 
-.listen( 3000 );
+app.get('/hola-mundo', function (req, res) {
+    res.send('Hola mundo desde su respectuva ruta')
+  })
 
-console.log('escuchando en el puerto',3000 );
+  app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/public/404.html')
+  })
+
+
+app.listen(3000)
---
+++ app_old.js
@@ -0,0 +1,33 @@
+const http = require ('http');
+
+http.createServer((req,res)=>{
+   
+//    res.writeHead(200, {'Content-Type': 'text/plain'})  // Archivos txt
+   
+//    res.writeHead(200, {'Content-Type': 'application/json'})  // lo muestra como codigo json 
+
+
+   // res.setHeader('Content-Disposition','attachment; filename=lista.csv');  // añade a los Header hacer un archivo que se va a descargar y ese es el nombre del archivo 'lista.csv'
+ //  res.writeHead(200, {'Content-Type': 'application/csv'})  //archivo excel u archivo csv 
+
+    // const persona ={
+    //     id: 1,
+    //     nombre: 'Fernando'
+    // }
+
+   // res.write(JSON.stringify (persona));
+   
+//    res.write('id, nombre\n');
+//    res.write('1, Ana\n');
+//    res.write('2, Carlos\n');
+//    res.write('3, Marcelo\n');
+//    res.write('4, Paola\n');
+
+    res.write('hola mundo');
+   res.end();
+   
+})
+
+.listen( 3000 );
+
+console.log('escuchando en el puerto',3000 );
